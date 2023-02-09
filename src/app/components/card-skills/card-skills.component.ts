b1050efea7bff61e5f5bba34860c6a970aa8462c
Alan Lazo <al4nlaz0@gmail.com>
2023-02-09 01:11:07 -0300

    updated card-skill.component.ts

--- src/app/components/card-skills/card-skills.component.ts
+++ src/app/components/card-skills/card-skills.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ISkill } from 'src/app/interfaces/ISkill.interface';
+import { ISkillDto } from 'src/app/interfaces/ISkillDto.interface';
 
 @Component({
   selector: 'app-card-skills',
@@ -10,9 +11,20 @@ import { ISkill } from 'src/app/interfaces/ISkill.interface';
 export class CardSkillsComponent implements OnInit {
 
   @Input() skills:ISkill[]=[]
+
+  logo!:File;
+
+  skillDto: ISkillDto = {
+    id_persona:1,
+    logo:'',
+    nombre:'',
+    rol:0
+  };
+
   constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+
   }
 
   public open(modal: any): void {
@@ -20,6 +32,15 @@ export class CardSkillsComponent implements OnInit {
   }
 
   agregarSkill(data:any){
-    console.log(data);
+    this.skillDto.rol = this.skills[0].rol.id
+    console.log(data.value);
+    console.log(this.skillDto)
+    console.log(this.logo);
+  }
+
+  saveLogo(data:any){
+    console.log(data.target.files[0])
+    this.skillDto.logo = data.target.files[0].name;
+    this.logo = data.target.files[0];
   }
 }
---
+++ src/app/interfaces/ISkillDto.interface.ts
@@ -0,0 +1,6 @@
+export interface ISkillDto {
+  id_persona:number;
+  logo:string;
+  nombre:string;
+  rol:number;
+}
