46d84a02e43e373d7def01e89db2971357b3de88
Alan Lazo <al4nlaz0@gmail.com>
2023-02-23 18:31:03 -0300

    added IProjectDto

---
+++ src/app/interfaces/IProjectDto.interface.ts
@@ -0,0 +1,10 @@
+export interface IProjectDto {
+      id_autor: number;
+      nombre: string;
+      descripcion: string;
+      repositorio: string;
+      demo: string;
+      image: string;
+      tecnologias: number[];
+}
+
