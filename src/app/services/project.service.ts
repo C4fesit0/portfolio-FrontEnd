aab48809d040bff24aa188ec54213ef953645725
Alan Lazo <al4nlaz0@gmail.com>
2023-02-25 17:27:40 -0300

    added createProject()

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IProject } from '../interfaces/IProject.interface';
+import { IProjectDto } from '../interfaces/IProjectDto.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,17 @@ import { IProject } from '../interfaces/IProject.interface';
 export class ProjectService {
 
   //apiURL = 'http://localhost:3000/projects';
-  apiURL='http://localhost:8080/proyecto/listar';
+  //apiURL='http://localhost:8080/proyecto/listar';
+
+  endPoint = "http://localhost:8080/proyecto/";
   constructor(private http:HttpClient) { }
 
   getProjects():Observable<IProject[]>{
-    return this.http.get<IProject[]>(this.apiURL);
+    return this.http.get<IProject[]>(this.endPoint+"listar");
+  }
+
+  createProject(data:IProjectDto):Observable<IProject>{
+    return this.http.post<IProject>(this.endPoint+"crear",data);
   }
 
 }
